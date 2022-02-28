ce86634d2069250c2882f52489c75a9b9e07d448
lfgodoi <eng.leonardogodoi@gmail.com>
2022-02-28 08:52:16 -0300

    Figures formatted and path for downloading the datasets made available

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 A full stack project for the analysis of vibration data using time and frequency techniques applied on public dataset signals.
 
 ## Development tools used in this project
-- FastAPI: backend
-- Docker: image containerization
-- Jenkins: CI/CD pipeline
+- FastAPI: backend.
+- Docker: image containerization.
+- Jenkins: CI/CD pipeline.
 
 ## Vibration analysis techniques used in this project
-- Statistical analysis (RMS, variance, peak-to-peak)
-- Frequency-domain analysis (fast Fourier transform)
+- Statistical analysis (RMS, variance, peak-to-peak).
+- Frequency-domain analysis (fast Fourier transform).
+
+## Downloading the vibration datasets
+In order to obtain the datasets for testing the application, please access the
+following shared folder, download and paste the available folders into
+the directory "vibration-analisis-interface/app/datasets".
+
+*https://drive.google.com/drive/folders/1nGa_2gpgtRKGYrosxdpbed2UQdmaI23q?usp=sharing*
--- app/sources/classes/visualization.py
+++ app/sources/classes/visualization.py
@@ -16,6 +16,9 @@ class Visualization:
 
     # Constructor
     def __init__(self):
+        self.max_spectrum = 20
+        self.colors = ["#0099ff", "#ff9900", "#009933", "#990099"]
+        self.maps = ["RMS", "Variance", "Peak-to-peak"]
         self.figures = {}
 
     # Time features plot
@@ -23,10 +26,11 @@ class Visualization:
         rows = len(time_features)
         cols = 1
         keys = list(time_features)
-        fig = make_subplots(rows=rows, cols=cols, subplot_titles=tuple(keys))
+        fig = make_subplots(rows=rows, cols=cols, subplot_titles=tuple(self.maps))
         for i in range(rows):
             key = keys[i]
-            fig.add_trace(go.Scatter(y=time_features[key], mode="lines"), row=i+1, col=1)
+            fig.add_trace(go.Scatter(y=time_features[key], mode="lines", line=dict(color=self.colors[i])), row=i+1, col=1)
+        fig.update_layout(autosize=True, showlegend=False)
         self.figures["time_features"] = fig.to_json()
         return fig
 
@@ -34,11 +38,21 @@ class Visualization:
     def plot_spectrum(self, frequency_features):
         spectrum = frequency_features["spectrum"]
         bins = frequency_features["bins"]
+        if len(spectrum) > self.max_spectrum:
+            indexes = np.linspace(0, len(spectrum) - 1, self.max_spectrum, dtype="int")
+            spectrum = spectrum[indexes]
         fig = go.Figure()
         y = np.zeros(spectrum.shape[1])
         for i in range(len(spectrum)):
-            fig.add_trace(go.Scatter3d(x=bins, y=y, z=spectrum[i], mode='lines'))
+            fig.add_trace(go.Scatter3d(x=bins, y=y, z=spectrum[i], mode='lines', line=dict(color=self.colors[-1])))
             y += 1
+        fig.update_layout(autosize=True, showlegend=False,
+        scene=dict(
+            xaxis=dict(title="Frequency (Hz)", zeroline=False),
+            yaxis=dict(title='Time (samples)', zeroline=False),
+            zaxis=dict(title='Amplitude', zeroline=False),
+            aspectmode='manual', 
+            aspectratio=dict(x=1.0, y=1.2, z=0.6)))
         self.figures["spectrum"] = fig.to_json()
         return fig
 
